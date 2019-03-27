536c020d2874b34da9e9f963c1b6b0c3e2dc56d4
chattopadhyayabhi <47132640+chattopadhyayabhi@users.noreply.github.com>
2019-03-27 00:35:27 -0400

    Add files via upload

---
+++ mylogistic.m
@@ -0,0 +1,30 @@
+
+data = load('data.txt');
+%bias term inclusion
+data = [ones(size(data, 1), 1), data];
+labels = load('labels.txt');
+
+%  training data uses the first 2000 rows
+train_labels = labels(1:2000);
+%testing data uses the rest
+test_data = data(2001:end, :);
+test_labels = labels(2001:end);
+
+
+n = [200, 500, 800, 1000, 1500, 2000];
+
+count = [];
+
+for i = amounts
+    weights = mylogistic_train(data(1:i, :), labels(1:i));
+    prediction =sigmf( test_data * weights,[1,0]);
+    
+    correct_pred = (test_labels == prediction);
+    accuracy = sum(correct_pred) / numel(correct_pred);
+    count = [count, accuracy];
+end
+
+plot(amounts, count, '-d')
+title(' Accuracy with respect to n')
+xlabel('n')
+ylabel(' accuracy')
---
+++ sparse.m
@@ -0,0 +1,35 @@
+clear
+load('ad_data.mat');
+
+% Add a column of 1 to add the bias 
+X_train = [ones(size(X_train, 1), 1) X_train];
+X_test = [ones(size(X_test, 1), 1) X_test];
+
+%list of regularization parameters
+parameters = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+
+%initialize number of feature and Area under the curve
+n_feature = zeros(size(parameters));
+area = zeros(size(parameters));
+count = 1;
+
+
+for par = parameters
+    [weights, bias] = logistic_l1_train(X_train, y_train, par);
+    n_feature(count) = nnz(weights);
+    predictions = X_test * weights;
+    [X, Y, T, AUC] = perfcurve(y_test, predictions, 1);
+    area(count) = AUC;
+    count = count + 1;
+end
+
+plot(parameters, n_feature)
+title(' number of feature selected vs. Regularization Parameter')
+xlabel(' Parameter')
+ylabel('number of feature selected ')
+
+figure
+plot(parameters, area)
+title(' AUC vs. Regularization Parameter')
+xlabel(' Parameter')
+ylabel('AUC')
